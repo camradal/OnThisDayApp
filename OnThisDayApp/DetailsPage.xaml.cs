a0dc6955c5ca87110160c35f3d507a716333b913
Alexey Kalinin <camradal@yahoo.com>
2012-01-26 18:54:37 -0800

    naive implementation of web browser loading control

--- OnThisDayApp/DetailsPage.xaml.cs
+++ OnThisDayApp/DetailsPage.xaml.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Net;
+using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
+using OnThisDayApp.Resources;
+using Utilities;
 
 namespace OnThisDayApp
 {
     public partial class DetailsPage : PhoneApplicationPage
     {
+        private bool navigating;
         private const string sourceUriFormat = @"http://en.wikipedia.org{0}";
 
         public DetailsPage()
         {
             InitializeComponent();
+
+            webBrowser1.Navigated += webBrowser1_Navigated;
+            webBrowser1.LoadCompleted += webBrowser1_LoadCompleted;
         }
 
-        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             string uri;
             if (NavigationContext.QueryString.TryGetValue("uri", out uri))
@@ -24,5 +31,29 @@ namespace OnThisDayApp
                 webBrowser1.Source = source;
             }
         }
+
+        #region Web browser
+
+        void webBrowser1_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (!navigating)
+            {
+                GlobalLoading.Instance.IsLoading = true;
+                GlobalLoading.Instance.LoadingText = Strings.Loading;
+            }
+            navigating = true;
+        }
+
+        private void webBrowser1_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            if (navigating)
+            {
+                GlobalLoading.Instance.IsLoading = false;
+                GlobalLoading.Instance.LoadingText = null;
+            }
+            navigating = false;
+        }
+
+        #endregion
     }
 }
--- OnThisDayApp/Resources/Strings.Designer.cs
+++ OnThisDayApp/Resources/Strings.Designer.cs
@@ -178,6 +178,15 @@ namespace OnThisDayApp.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Loading....
+        /// </summary>
+        public static string Loading {
+            get {
+                return ResourceManager.GetString("Loading", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to about.
         /// </summary>
         public static string MenuItemAbout {
